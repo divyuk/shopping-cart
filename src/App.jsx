530f32bbbd858823a8278dfb0db24a54931caa42
divyuk <div14kes@gmail.com>
2024-11-03 11:11:11 +0530

    Added comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Products from "./components/Products";
 import Cart from "./components/Cart";
@@ -8,7 +5,7 @@ import Cart from "./components/Cart";
 function App() {
   return (
     <>
-    {/* Product and Cart component */}
+      {/* Product and Cart component */}
       <Products />
       <Cart />
     </>
